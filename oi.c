505f58c5ab8368c39b2c9a631a4aa6644c48b716
Bruna Mayumi Hori <brunahori19@gmail.com>
2016-09-28 23:19:10 -0300

    tentando

--- oi.c
+++ oi.c
@@ -36,7 +36,7 @@
 #define CORAMARELO (makecol(255,255,100))
 #define CORVERMELHO (makecol(255, 0, 0))
 
-#define IMAGENAME "oiteste.bmp" /* nome do arquivo de imagem */
+#define IMAGENAME "oiteste2.bmp" /* nome do arquivo de imagem */
 
 #include <stdio.h>
 #include <allegro.h>
