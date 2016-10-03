cf96ef31b5b61a6633594e169654e6ef8756b728
Bruna Mayumi Hori <brunahori19@gmail.com>
2016-10-03 00:19:50 -0300

    movendo x?

--- oi.c
+++ oi.c
@@ -36,7 +36,7 @@
 #define CORAMARELO (makecol(255,255,100))
 #define CORVERMELHO (makecol(255, 0, 0))
 
-#define IMAGENAME "oiteste5.bmp" /* nome do arquivo de imagem */
+#define IMAGENAME "oiteste6.bmp" /* nome do arquivo de imagem */
 
 #include <stdio.h>
 #include <allegro.h>
@@ -61,8 +61,8 @@ int main(void)
     }
     
     circlefill(buff, 160, 120, 100, CORAMARELO);
-    circle(buff,  80, 60, 100, CORAZUL);
-    textprintf_ex(buff, font, 100, 100, CORVERDE, CORPRETO, "Teste do circulo.");
+    circle(buff, 160, 60, 100, CORAZUL);
+    textprintf_ex(buff, font, 100, 100, CORVERDE, CORPRETO, "MOVENDO ALGUEM!");
 
     save_bitmap(IMAGENAME, buff, pal);
     destroy_bitmap(buff);
